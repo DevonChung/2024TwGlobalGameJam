598716df3e785599881001f0359fd81a14fca66f
DevonChung <dp.atelier5@gmail.com>
2024-01-27 20:01:28 +0800

    Add random item function. Add girl friend's home prefab

--- CrossRoad/Assets/Script/RandomItemHolder.cs
+++ CrossRoad/Assets/Script/RandomItemHolder.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class RandomItemHolder : MonoBehaviour
 {
-    
+    public List<GameObject> ItemPool;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        int iPoolSize = ItemPool.Count;
+    
+        if (iPoolSize > 0)
+        {
+            int idx = Random.Range(0, iPoolSize);
+            
+            GameObject myObj = Instantiate(ItemPool[idx], this.transform.position, this.transform.rotation);
+        }
     }
 
     // Update is called once per frame
