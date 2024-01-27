1a7499d800714ab9168cf7e5bd67aa6b499b654f
DevonChung <dp.atelier5@gmail.com>
2024-01-27 16:04:05 +0800

    no message

--- CrossRoad/Assets/Script/MyGameManager.cs
+++ CrossRoad/Assets/Script/MyGameManager.cs
@@ -5,19 +5,22 @@ using UnityEngine.UI;
 
 public class MyGameManager : MonoBehaviour
 {
-    public enum CrossRoadGameStatus { Paused, InGame};
+    public enum CrossRoadGameStatus { Paused, InGame, EndGame};
 
-    public float timeLeft = 90;
+    public float timeLeft = 90.0f;
     public int StartMoney = 1000;
     protected int CurrentMony = 1000;
     protected CrossRoadGameStatus currentState = CrossRoadGameStatus.Paused;
     public GameObject StartPos;
     protected GameObject Hero;
 
+    public UIManager myUIManager;
+
     // Start is called before the first frame update
     void Start()
     {
-        currentState = CrossRoadGameStatus.Paused;
+        //currentState = CrossRoadGameStatus.Paused;
+        currentState = CrossRoadGameStatus.InGame;
         Hero = GameObject.FindGameObjectWithTag("Player");
         if (StartPos != null)
         { 
@@ -28,6 +31,27 @@ public class MyGameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (currentState == CrossRoadGameStatus.InGame)
+        {
+            if (timeLeft > 0)
+            {
+                timeLeft -= Time.deltaTime;
+            }
+            else
+            {
+                timeLeft = 0;
+            }
+            if (myUIManager != null)
+            {
+                myUIManager.SetTime(timeLeft);
+            }
+
+            if (timeLeft <= 0 )
+            {
+                currentState = CrossRoadGameStatus.EndGame;
+                myUIManager.SetText("Time is over");
+            }
+
+        }
     }
 }
